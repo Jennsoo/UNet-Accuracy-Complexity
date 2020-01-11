b552e31e068e980d9ad55f3567d9cc2417be4afa
JYSong <634973606@qq.com>
2020-01-11 18:30:41 +0800

    Add files via upload

--- mypath.py
+++ mypath.py
@@ -2,7 +2,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'ctchest':
-            return '/'	# path of the dataset
+            return '/'	# path of index file.txt of dataset
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
